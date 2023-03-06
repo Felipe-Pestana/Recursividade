c9a7f86e6faf988e56742f7ac58a23e8bf4aef91
Felipe Pestana <pestana@sitic.com.br>
2023-03-06 13:14:13 -0300

    Add project files.

---
+++ Argumentos/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+        int resposta;
+
+        Console.WriteLine("Informe um valor: ");
+        num = int.Parse(Console.ReadLine());
+
+        resposta = fat(num);
+        Console.WriteLine("O fatorial de " + num + " é: " + resposta);
+
+        int fat (int valor)
+        {
+            if (valor == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return (valor * fat(valor-1));
+            }
+        }
+    }
+}
